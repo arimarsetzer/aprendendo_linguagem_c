4e5f08dff06110ced4c09fb1062d631be3c8bdb1
arimarsetzer <arimar.carvalho94@gmail.com>
2023-02-16 17:47:11 -0300

    aula08_variável_int

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 
 int main()
 {
-    printf("Hello, world!");
-    return 0;
+    // imprime o cabeçalho do nosso jogo
+    printf("**************************************************************\n");
+    printf("* Welcome to our guessing game *\n");
+    printf("**************************************************************\n");
+
+    int numerosecreto = 42;
+
+    printf("O número %d é o secreto. Não conta pra ninguém!", numerosecreto);
 }
