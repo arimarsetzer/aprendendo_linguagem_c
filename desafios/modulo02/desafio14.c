3cac2ffda66a51399e2119482c65d429405d5578
arimarsetzer <arimar.carvalho94@gmail.com>
2023-02-17 18:47:17 -0300

    modulo02_desafio14

---
+++ desafios/modulo02/desafio14.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    // print game header
+    printf("**************************************************************\n");
+    printf("* Print from 1 to 100 using FOR loop function *\n");
+    printf("**************************************************************\n");
+
+    for (int i = 1; i <= 100; i++)
+    {
+        printf("%d\n", i);
+    }
+}
