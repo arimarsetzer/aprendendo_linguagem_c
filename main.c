c01f380f01f477f70c9c393a8ee64a4ffcada5a4
arimarsetzer <arimar.carvalho94@gmail.com>
2023-02-17 21:51:53 -0300

    modulo04_aula01_switch

--- main.c
+++ main.c
@@ -17,8 +17,34 @@ int main()
     int tries = 1;
     double points = 1000;
 
-    while (1)
+    int sucess = 0;
+
+    int level;
+    printf("What difficult level do you want to play?\n");
+    printf("(1) Easy (2) Medium (3) Hard\n\n");
+    printf("Choose the difficult level: ");
+    scanf("%d", &level);
+
+    int attemptsnumber;
+
+    switch (level)
     {
+    case 1:
+        attemptsnumber = 20;
+        break;
+
+    case 2:
+        attemptsnumber = 15;
+        break;
+
+    default:
+        attemptsnumber = 6;
+        break;
+    }
+
+    for (int i = 1; i <= attemptsnumber; i++)
+    {
+
         printf("\n");
         printf("Guess n° %d\n", tries);
         printf("What's your guess? ");
@@ -32,12 +58,11 @@ int main()
             continue;
         }
 
-        int sucess = (guess == secretnumber);
+        sucess = (guess == secretnumber);
         int higher = guess > secretnumber;
 
         if (sucess)
         {
-            printf("Congratulations, you guessed the correct number!!\n");
             break;
         }
 
@@ -55,8 +80,17 @@ int main()
 
         points = points - lostpoints;
     }
+
     printf("\n");
     printf("Game over!!\n");
-    printf("You won in the %d attempt!\n", tries);
-    printf("Total points: %.2f\n", points);
+
+    if (sucess)
+    {
+        printf("You won in the %d attempt!\n", tries);
+        printf("Total points: %.2f\n", points);
+    }
+    else
+    {
+        printf("You lost! The value was %d! Try again!\n", secretnumber);
+    }
 }
